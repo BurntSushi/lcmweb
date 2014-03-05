bd0c76a50232af234e8a018d9d8e60f41cb5dd49
Andrew Gallant <jamslam@gmail.com>
2014-03-04 22:38:55 -0500

    Don't clear the error on successful noop.
    Leave breadcrumbs explaining why.

--- static/js/session-ping.js
+++ static/js/session-ping.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
         jpost('/noop').always(function(r) {
             if (!is_success(r)) {
                 flash_response_error(r);
-            } else {
-                flash_hide_error();
             }
+            // Don't hide the error if successful since there may be
+            // an unrelated error already on the screen.
         });
     }
     window.setInterval(ping, 30 * 1000);
